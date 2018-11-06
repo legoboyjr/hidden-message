0e1811ba9f4e6106fdf4c616b8f72c353dae48c1
Alexander Thompson <legoboyjr@gmail.com>
2018-11-05 20:59:58 -0500

    finished button

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      it works
+      <header className="app-header">
+            <div className="top-left">
+            <a href="https://www.youtube.com/watch?v=hvvjiE4AdUI" class="btn btn-lg" role="button" aria-pressed="true">help me!!!!!!</a>
+            </div>
+      </header>
+      <main className="app-main">
+        
+      </main>
+      <footer className="app-footer">
+
+      </footer>
       </div>
     );
   }
