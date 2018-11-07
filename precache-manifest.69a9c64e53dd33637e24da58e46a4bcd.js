43c6b73575ac31563f62dd6e941a822a4b6f9a71
Alexander Thompson <legoboyjr@gmail.com>
2018-11-06 19:34:27 -0500

    Updates

---
+++ precache-manifest.69a9c64e53dd33637e24da58e46a4bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3670dbab899324334efce9ca2611305",
+    "url": "/hidden-message/static/media/sad.d3670dba.png"
+  },
+  {
+    "revision": "cf18b990da0fafdb6360",
+    "url": "/hidden-message/static/js/runtime~main.cf18b990.js"
+  },
+  {
+    "revision": "ddf150dcbf29b2e13b3e",
+    "url": "/hidden-message/static/js/main.ddf150dc.chunk.js"
+  },
+  {
+    "revision": "2ed623907bf5a1394b78",
+    "url": "/hidden-message/static/js/1.2ed62390.chunk.js"
+  },
+  {
+    "revision": "ddf150dcbf29b2e13b3e",
+    "url": "/hidden-message/static/css/main.7151c348.chunk.css"
+  },
+  {
+    "revision": "2ed623907bf5a1394b78",
+    "url": "/hidden-message/static/css/1.02307739.chunk.css"
+  },
+  {
+    "revision": "d364d7cbb89ef6c6df074e1377ce78ab",
+    "url": "/hidden-message/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hidden-message/precache-manifest.69a9c64e53dd33637e24da58e46a4bcd.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hidden-message/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
